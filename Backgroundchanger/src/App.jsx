c658c8e241eeef7f68d8c7173b57141a5b331458
unknown <tulsaroad.computer.anum@gmail.com>
2024-06-22 14:01:30 +0500

    Backgroundchanger

--- Backgroundchanger/src/App.jsx
+++ Backgroundchanger/src/App.jsx
@@ -8,18 +8,21 @@ function App() {
     
     <div className='h-screen duration-200 w-full' style={{backgroundColor: color}} >
        <div className=' flex flex-wrap inset-x-0 fixed bottom-8 justify-center'>
+     
      <div className='flex flex-wrap px-2 py-2 bg-white shadow-lg rounded-3xl  justify-center'>
+     
      <button className='px-4 py-3 m-2 shadow-lg rounded-2xl' 
      onClick={() => setColor("Red")}             //use of call back function 
-     style={{backgroundColor: 'red', color: 'white'}}>
-      Red
-      </button>   
+     style={{backgroundColor: 'red', color: 'white'}}> Red </button>   
+     
      <button className='px-4 py-3 m-2 shadow-lg rounded-2xl' 
      onClick={() => setColor("Green")} 
      style={{backgroundColor: 'green', color: 'white'}}>Green</button>
+     
      <button className='px-4 py-3 m-2 shadow-lg rounded-2xl' 
      onClick={() => setColor("blue")} 
      style={{backgroundColor: 'Blue', color: 'white'}}>Blue</button>
+     
      <button className='px-4 py-3 m-2 shadow-lg rounded-2xl' 
      onClick={() => setColor("olive")} 
      style={{backgroundColor: 'olive', color: 'white'}}>Olive</button>
